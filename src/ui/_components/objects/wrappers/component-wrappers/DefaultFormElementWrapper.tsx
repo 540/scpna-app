97c5aa70e8246d81660bfb0b4033d58213081fd2
David Rodriguez Garcia <david@MacBook-Pro-de-infotec.local>
2022-05-18 18:18:15 +0200

    BugFix: Justify-content style of DefaultFormElementWrapper was not getting applied because of typo

--- src/ui/_components/objects/wrappers/component-wrappers/DefaultFormElementWrapper.tsx
+++ src/ui/_components/objects/wrappers/component-wrappers/DefaultFormElementWrapper.tsx
@@ -4,6 +4,6 @@ import styled from '@emotion/styled'
 export const DefaultFormElementWrapper = styled.div`
   padding: 20px;
   display: flex;
-  justify-content: 'center';
-  align-items: 'center';
+  justify-content: center;
+  align-items: center;
 `
