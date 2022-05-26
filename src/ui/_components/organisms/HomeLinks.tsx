5014a29350f6f4ec6031a0121a1b39838afa5114
migtarx <migtarx@gmail.com>
2022-05-26 15:09:20 +0200

    feat: Added link ref to social icons

--- src/ui/_components/organisms/HomeLinks.tsx
+++ src/ui/_components/organisms/HomeLinks.tsx
@@ -3,12 +3,22 @@ import { LinkGoldButton } from '../atoms/buttons/LinkGoldButton'
 import LiveHelpIcon from '@mui/icons-material/LiveHelp'
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer'
 import EventAvailableIcon from '@mui/icons-material/EventAvailable'
+import { SocialBox } from '../../_components/molecules/'
 
 const HomeLinksWrapper = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
   width: 100%;
+`
+
+const Socials = styled.div`
+  display: inline-block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 190px;
+  max-width: 190px;
+  margin-top: 2%;
 `
 
 export const HomeLinks = ({}: {}) => {
@@ -23,6 +33,9 @@ export const HomeLinks = ({}: {}) => {
       <LinkGoldButton icon={<EventAvailableIcon />} link="/schedules">
         Horario
       </LinkGoldButton>
+      <Socials>
+        <SocialBox />
+      </Socials>
     </HomeLinksWrapper>
   )
 }
