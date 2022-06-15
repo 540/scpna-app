2771f55f1cdb23a1d0f4597e7efec6b6d2d981cf
David Rodriguez Garcia <david@MacBook-Pro-de-infotec.local>
2022-06-15 19:06:03 +0200

    added the test for properly choosing an option on the select, even though it throws a warning about not using act() as a wrapper for formik state change, the last test should be commented, and it needs the former one for it to be completed. So first solve the select one, and later, the todo one that englobes them all

--- src/__tests__/FormValidation.ispec.tsx
+++ src/__tests__/FormValidation.ispec.tsx
@@ -1,8 +1,53 @@
-import { render, screen, within } from '@testing-library/react'
+import { fireEvent, render, screen, within, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import AskPage from '../../pages/ask'
+
 describe('form', () => {
+  const talks = [
+    { text: 'Opcion 1', value: 'v1' },
+    { text: 'Opcion 2', value: 'v2' },
+    { text: 'Opcion 3', value: 'v3' }
+  ]
+
   it('select displays properties on click', async () => {
+    //cleanup is done for us
+    render(<AskPage talks={talks} />)
+    const select = screen.getByRole('button', { name: /t-label_talk/i })
+
+    await userEvent.click(select)
+
+    const options = screen.getAllByRole('option')
+    const optionsText = options.map(elem => elem.textContent)
+
+    talks.forEach(elem => {
+      expect(optionsText).toContain(elem.text)
+    })
+  })
+
+  it('choose option from select', async () => {
+    //cleanup is done for us
+    render(<AskPage talks={talks} />)
+    const select = screen.getByRole('button', { name: /t-label_talk/i })
+
+    await userEvent.click(select)
+    const listBox = screen.getByRole('listbox')
+
+    const options = within(listBox).getAllByRole('option')
+
+    fireEvent(
+      options[2],
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    await waitFor(() => {
+      expect(select.textContent).toBe('Opcion 3')
+    })
+  })
+
+  it.todo('form is filled properly', async () => {
     const talks = [
       { text: 'Opcion 1', value: 'v1' },
       { text: 'Opcion 2', value: 'v2' },
@@ -11,17 +56,32 @@ describe('form', () => {
 
     render(<AskPage talks={talks} />)
 
+    const arbitraryName = 'I am the name'
+    const arbitraryEmail = 'I am the email'
+    const arbitraryQuestion = 'I am the question'
+
+    const nameInput = screen.getByRole('textbox', {
+      name: /t-label_name/i
+    })
+    const emailInput = screen.getByRole('textbox', {
+      name: /t-label_email/i
+    })
+    const questionInput = screen.getByRole('textbox', {
+      name: /t-label_question/i
+    })
     const select = screen.getByRole('button', { name: /t-label_talk/i })
 
+    await userEvent.type(nameInput, arbitraryName)
+    await userEvent.type(emailInput, arbitraryEmail)
+    await userEvent.type(questionInput, arbitraryQuestion)
     await userEvent.click(select)
+
     const listBox = screen.getByRole('listbox')
 
-    const options = within(listBox).getAllByRole('option')
+    //on hold until we are able to select an option from the select
 
-    const optionsText = options.map(elem => elem.textContent)
-
-    talks.forEach(elem => {
-      expect(optionsText).toContain(elem.text)
-    })
+    expect(nameInput.value).toBe(arbitraryName)
+    expect(emailInput.value).toBe(arbitraryEmail)
+    expect(questionInput.value).toBe(arbitraryQuestion)
   })
 })
