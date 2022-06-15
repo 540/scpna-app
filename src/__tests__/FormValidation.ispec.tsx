f330716e134632b0d14fd61e8882d8f17ba4a426
David Rodriguez Garcia <david@MacBook-Pro-de-infotec.local>
2022-06-15 12:19:13 +0200

    select properties test

--- src/__tests__/FormValidation.ispec.tsx
+++ src/__tests__/FormValidation.ispec.tsx
@@ -1,40 +1,27 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import AskPage from '../../pages/ask'
-
-describe('proper talks have been passed all the way through and are displayed properly', () => {
-  it('select has the correct properties', () => {
+describe('form', () => {
+  it('select displays properties on click', async () => {
     const talks = [
-      { text: 't1', value: 'v1' },
-      { text: 't2', value: 'v2' },
-      { text: 't3', value: 'v3' }
+      { text: 'Opcion 1', value: 'v1' },
+      { text: 'Opcion 2', value: 'v2' },
+      { text: 'Opcion 3', value: 'v3' }
     ]
-    render(<AskPage talks={talks} />)
-    
-  })
-})
 
-describe('Hello test', () => {
-  it('renders hello', () => {
-    const talks = [
-      { text: 't1', value: 'v1' },
-      { text: 't2', value: 'v2' },
-      { text: 't3', value: 'v3' }
-    ]
     render(<AskPage talks={talks} />)
 
-    const [nameInput, emailInput, questionInput] = screen.getAllByRole('textbox')
-    userEvent.type(nameInput, 'David')
-    userEvent.type(emailInput, 'David@gmail.com')
-    userEvent.type(questionInput, 'Tengo una duda...')
+    const select = screen.getByRole('button', { name: /t-label_talk/i })
 
-    // const [nameInput, emailInput, questionInput] = screen.getAllByRole('')
-    console.log(nameInput.name, emailInput.name, questionInput.name)
-    // const nameInput = screen.getByRole('textbox', { name: /t-label_name/i })
-    // const nameInput = screen.getByRole('textbox', { name: /t-label_name/i })
-    // const nameInput = screen.getByRole('textbox', { name: /t-label_name/i })
-    // screen.debug(nameInput)
+    await userEvent.click(select)
+    const listBox = screen.getByRole('listbox')
 
-    expect(1).toBe(1)
+    const options = within(listBox).getAllByRole('option')
+
+    const optionsText = options.map(elem => elem.textContent)
+
+    talks.forEach(elem => {
+      expect(optionsText).toContain(elem.text)
+    })
   })
 })
